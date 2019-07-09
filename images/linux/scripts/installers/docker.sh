6c737db4db795fdc508a21e3910e973db78c600c
Parin Sanghavi <32961811+pasangh@users.noreply.github.com>
2019-07-09 19:02:19 -0400

    Update docker.sh

--- images/linux/scripts/installers/docker.sh
+++ images/linux/scripts/installers/docker.sh
@@ -2,36 +2,11 @@
 ################################################################################
 ##  File:  docker.sh
 ##  Team:  CI-Platform
-##  Desc:  Installs the correct version of docker onto the image, and pulls
-##         down the default docker image used for building on ubuntu
+##  Desc:  Pulls down the docker images used for building on ubuntu
 ################################################################################
 
 source $HELPER_SCRIPTS/apt.sh
 source $HELPER_SCRIPTS/document.sh
-
-DOCKER_PACKAGE=moby
-DEFAULT_DOCKER_IMAGE=microsoft/vsts-agent:ubuntu-16.04-docker-17.12.0-ce-standard
-
-LSB_CODENAME=$(lsb_release -cs)
-
-## Check to see if docker is already installed
-echo "Determing if Docker ($DOCKER_PACKAGE) is installed"
-if ! IsInstalled $DOCKER_PACKAGE; then
-    echo "Docker ($DOCKER_PACKAGE) was not found. Installing..."
-    apt-get remove -y moby-engine mobi-cli
-    apt-get update
-    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-    curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/microsoft-prod.list
-    apt-get update
-    apt-get install -y moby-engine mobi-cli
-else
-    echo "Docker ($DOCKER_PACKAGE) is already installed"
-fi
-
-## Always pull down latest docker image, as it will no-op if it already exists
-echo "Pulling down latest Docker image ($DEFAULT_DOCKER_IMAGE)"
-docker pull $DEFAULT_DOCKER_IMAGE
 
 # Cache popular docker images
 docker pull node:8
@@ -48,7 +23,8 @@ docker pull alpine:3.8
 docker pull alpine:3.9
 docker pull alpine:3.10
 
-## Add version information to the metadata file
-echo "Documenting Docker version"
-DOCKER_VERSION=`docker -v`
-DocumentInstalledItem "Docker ($DOCKER_VERSION)"
+docker images
+
+echo "Documenting Docker images cached"
+docker_images=$(docker images)
+DocumentInstalledItem "Docker ($docker_images)"
