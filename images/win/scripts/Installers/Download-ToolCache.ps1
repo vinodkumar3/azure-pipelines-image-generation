c7f50ce262711887836bb723ee1c71c4c8eb053a
Aleksandr Chebotov <47745270+al-cheb@users.noreply.github.com>
2019-09-23 13:29:30 +0300

    Update Download-ToolCache.ps1

--- images/win/scripts/Installers/Download-ToolCache.ps1
+++ images/win/scripts/Installers/Download-ToolCache.ps1
@@ -5,40 +5,42 @@
 ################################################################################
 
 Function Install-NpmPackage {
-	[CmdletBinding()]
-	param(
-		[Parameter(Mandatory=$true)]
-		[System.String[]]
-		$Name,
-		[System.String]
-		$NpmRegistry="https://buildcanary.pkgs.visualstudio.com/PipelineCanary/_packaging/hostedtoolcache/npm/registry/"
-	)
-	
-	foreach($packageName in $Name) {
-		Write-Host "Installing npm $packageName package from '$NpmRegistry'"
-		$null = npm install $packageName --registry=$NpmRegistry *>&1
-	}
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [System.String[]]
+        $Name,
+        [System.String]
+        $NpmRegistry="https://buildcanary.pkgs.visualstudio.com/PipelineCanary/_packaging/hostedtoolcache/npm/registry/"
+    )
+
+    foreach($packageName in $Name) {
+        Write-Host "Installing npm '$packageName' package from '$NpmRegistry'"
+        $null = npm install $packageName --registry=$NpmRegistry *>&1
+    }
 }
 
+# HostedToolCache Path
 $Dest = "C:/"
 $Path = "hostedtoolcache/windows"
 $ToolsDirectory = $Dest + $Path
 
+# Define AGENT_TOOLSDIRECTORY environment variable
+$env:AGENT_TOOLSDIRECTORY = $ToolsDirectory
 setx AGENT_TOOLSDIRECTORY $ToolsDirectory /M
 
-$env:AGENT_TOOLSDIRECTORY = $ToolsDirectory
- 
+# Install Python ToolCache
 $PythonVersionsToolcacheInstall = @(
-	"toolcache-python-windows-x64@2.7"
-	"toolcache-python-windows-x86@2.7"
-	"toolcache-python-windows-x64@3.4"
-	"toolcache-python-windows-x86@3.4"
-	"toolcache-python-windows-x64@3.5"
-	"toolcache-python-windows-x86@3.5"
-	"toolcache-python-windows-x64@3.6"
-	"toolcache-python-windows-x86@3.6"
-	"toolcache-python-windows-x64@3.7"
-	"toolcache-python-windows-x86@3.7"
+    "toolcache-python-windows-x64@2.7"
+    "toolcache-python-windows-x86@2.7"
+    "toolcache-python-windows-x64@3.4"
+    "toolcache-python-windows-x86@3.4"
+    "toolcache-python-windows-x64@3.5"
+    "toolcache-python-windows-x86@3.5"
+    "toolcache-python-windows-x64@3.6"
+    "toolcache-python-windows-x86@3.6"
+    "toolcache-python-windows-x64@3.7"
+    "toolcache-python-windows-x86@3.7"
 )
 
 Install-NpmPackage -Name $PythonVersionsToolcacheInstall
@@ -46,5 +48,3 @@ Install-NpmPackage -Name $PythonVersionsToolcacheInstall
 #junction point from the previous Python2 directory to the toolcache Python2
 $python2Dir = (Get-Item -Path ($ToolsDirectory + '/Python/2.7*/x64')).FullName
 cmd.exe /c mklink /d "C:\Python27amd64" "$python2Dir"
-
-
