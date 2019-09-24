f631e7447e99ad5815a539c150ee0e99b71b0e2e
Alejandro Pauly <alpauly@microsoft.com>
2019-09-24 07:15:36 -0400

    Fix wait-process that's failing after latest VS2019 update.

--- images/win/scripts/Installers/Vs2019/Install-VS2019.ps1
+++ images/win/scripts/Installers/Vs2019/Install-VS2019.ps1
@@ -159,7 +159,7 @@ $version = $catalog.info.id
 Write-Host "Visual Studio version" $version "installed"
 
 # Initialize Visual Studio Experimental Instance
-&"C:\Program Files (x86)\Microsoft Visual Studio\2019\$ReleaseInPath\Common7\IDE\devenv.exe" /RootSuffix Exp /ResetSettings General.vssettings /Command File.Exit | Wait-Process
+&"C:\Program Files (x86)\Microsoft Visual Studio\2019\$ReleaseInPath\Common7\IDE\devenv.exe" /RootSuffix Exp /ResetSettings General.vssettings /Command File.Exit
 
 # Updating content of MachineState.json file to disable autoupdate of VSIX extensions
 $newContent = '{"Extensions":[{"Key":"1e906ff5-9da8-4091-a299-5c253c55fdc9","Value":{"ShouldAutoUpdate":false}},{"Key":"Microsoft.VisualStudio.Web.AzureFunctions","Value":{"ShouldAutoUpdate":false}}],"ShouldAutoUpdate":false,"ShouldCheckForUpdates":false}'
