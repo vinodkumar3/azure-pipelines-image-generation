d28b37390ffc5bed944233b21fb57616526581d0
Lydia Li <yiqli@microsoft.com>
2019-10-05 12:24:27 -0400

    only add Update-MpSignature after pop-location

--- images/win/scripts/Installers/Vs2019/Run-Antivirus.ps1
+++ images/win/scripts/Installers/Vs2019/Run-Antivirus.ps1
@@ -18,9 +18,8 @@ Push-Location "C:\Program Files\Windows Defender"
 # Tell Defender to use 100% of the CPU during the scan
 Set-MpPreference -ScanAvgCPULoadFactor 100
 
-Update-MpSignature
 # Full Scan
-.\MpCmdRun.exe -Scan -ScanType 2 -SignatureUpdate
+.\MpCmdRun.exe -Scan -ScanType 2
 Pop-Location
 
 Update-MpSignature
